29d89459564de4c9b45ae05d13ca6c3e8e348114
scriptonist <aravind.techbridge@gmail.com>
2016-11-08 11:35:59 +0530

    commented  LUIS Part

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var builder = require('botbuilder');
 var restify = require('restify');
 var Client = require('node-rest-client').Client;
 var dotenv = require('dotenv');
-var model = process.env.LUIS_MODEL;
-var recogonizer = new builder.LuisRecognizer(model);
+// var model = process.env.LUIS_MODEL;
+// var recogonizer = new builder.LuisRecognizer(model);
 var dialog = new builder.IntentDialog();
 
 dotenv.load();
