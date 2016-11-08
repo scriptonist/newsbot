fcff7668d548d035d964b6cbb0b72da6733bbe35
scriptonist <aravind.techbridge@gmail.com>
2016-11-08 11:37:29 +0530

    change bit

--- app.js
+++ app.js
@@ -50,9 +50,6 @@ bot.dialog('/showpublishers',[
     
 ]);
 dialog.onDefault([
-    function (session) {
-        builder.Prompts.text(session, 'Which Publisher ? ');
-    },
     function (session,results) {
         var source = results.response;
         var client = new Client();
